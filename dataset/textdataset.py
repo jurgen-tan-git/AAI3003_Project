0c712619d9e939735a140b1d2cef13b65dec04d1
Christopher Kok <ForceLightning@users.noreply.github.com>
2024-03-23 15:31:09 +0800

    fix: forgot to add the changes to the dataset file

--- dataset/textdataset.py
+++ dataset/textdataset.py
@@ -1,5 +1,6 @@
 import os
 
+import pandas as pd
 import torch
 from sklearn.preprocessing import LabelEncoder
 from torch.utils.data import Dataset
@@ -17,6 +18,7 @@ class ArticleDataset(Dataset):
 
     Attributes:
         articles_dir (str or os.PathLike): The directory path where the articles are stored.
+        labelled_csv (str or os.PathLike): The path to the labelled CSV file.
         tokenizer (PreTrainedTokenizerBase): The tokenizer used to tokenize the text.
         max_length (int): The maximum length of the tokenized input.
         articles (list): A list of article texts.
@@ -34,6 +36,7 @@ class ArticleDataset(Dataset):
     def __init__(
         self,
         articles_dir: str | os.PathLike[str],
+        labelled_csv: str | os.PathLike[str],
         tokenizer: PreTrainedTokenizerBase,
         max_length: int,
     ) -> None:
@@ -41,8 +44,10 @@ class ArticleDataset(Dataset):
         self.articles_dir = articles_dir
         self.tokenizer = tokenizer
         self.max_length = max_length
+        self.labelled_csv = labelled_csv
         self.articles = []
         self.labels = []
+        self.targets = []
         self.categories = []
         self.label_encoder = LabelEncoder()
         self._init_dataset()
@@ -51,18 +56,24 @@ class ArticleDataset(Dataset):
         """
         Initializes the dataset by loading and preprocessing the articles.
         """
-        self.categories = os.listdir(self.articles_dir)
-        for category in self.categories:
-            for article in os.listdir(os.path.join(self.articles_dir, category)):
-                with open(
-                    os.path.join(self.articles_dir, category, article),
-                    "r",
-                    encoding="utf-8",
-                ) as f:
-                    text = f.read()
-                    self.articles.append(text)
-                    self.labels.append(category)
-        self.labels = self.label_encoder.fit_transform(self.labels)
+        df = pd.read_csv(self.labelled_csv)
+        for root, _, files in os.walk(self.articles_dir):
+            for file in files:
+                if file.endswith(".txt"):
+                    with open(os.path.join(root, file), "r", encoding="utf-8") as f:
+                        text = f.read()
+                        df.loc[df["File"] == file, "Text"] = text
+
+        for _, row in df.iterrows():
+            targets = row[2:]
+            labels = df.columns[2:][targets == 1]
+            labels = list(map(lambda x: x.replace("-", " "), labels))
+            text = row["Text"]
+            if text:
+                self.articles.append(text)
+                self.labels.append(labels)
+                self.targets.append(targets)
+        self.categories = df.columns[2:].to_list()
 
     def __len__(self):
         """
@@ -78,10 +89,11 @@ class ArticleDataset(Dataset):
             idx (int): The index of the article.
 
         Returns:
-            dict: A dictionary containing the tokenized input and label.
+            dict: A dictionary containing the tokenized input and the multilabel
+                targets.
         """
         text = self.articles[idx]
-        label = self.labels[idx]
+        target = self.targets[idx]
         inputs = self.tokenizer(
             text,
             return_tensors="pt",
@@ -96,4 +108,4 @@ class ArticleDataset(Dataset):
             "token_type_ids": (
                 inputs.token_type_ids if "token_type_ids" in inputs else None
             ),
-        }, torch.tensor(label)
+        }, torch.tensor(target)
