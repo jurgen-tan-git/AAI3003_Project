17a4127917c8654ae67e2ae8de7eb33c3ee419d3
jurgen-tan-git <56992789+jurgen-tan-git@users.noreply.github.com>
2024-03-25 18:01:20 +0800

    Called prediction code and summarisation using multiprocessing

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from summarise import summarize
 from scraper import *
+from deploy_predict import *
+import multiprocessing
 
 st.title('TLDR News Summariser')
 
@@ -11,9 +13,11 @@ if st.button('Summarise Article'):
     if url:
         # Scrape the article
         article = scrape_article(url)
-        print(article)
         article_text = ' '.join(article)
-        print(article_text)
+
+        # Predict the category of the article using multiprocessing
+        predict_process = multiprocessing.Process(target=predict_on_model, args=(article,))
+        predict_process.start()
 
         # Summarise the article
         article_text = summarize(article_text)
@@ -22,7 +26,16 @@ if st.button('Summarise Article'):
             st.write('Failed to scrape the article. Please enter a valid URL.')
             st.stop()
         else:
-            # result here
+            # Wait for the prediction process to finish
+            predict_process.join()
+
+            # Display the summarised article
             st.write(article_text)
+
+            # Get predictions and display the categories
+            predictions = predict_process.get()
+            labels = get_label(predictions)
+            st.write('The article is classified under the following categories:', labels)
+
     else:
         st.write('Please enter a valid URL.')
